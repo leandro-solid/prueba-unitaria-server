84d4bc46429421ac5432d1d7262e34381deae290
Leandro Loiacono <leandro@solidteam.es>
2023-10-09 18:34:35 +0200

    [ADD] routes for testing

--- src/index.js
+++ src/index.js
@@ -4,9 +4,20 @@ const app = express();
 
 const port = 3000;
 
-app.get("/home", (req, res) => {
-  console.log("Esto es un GET a '/home'");
-  res.send("Esto es un GET a '/home' ");
+app.get("/*", (res, req) => {
+  console.log("Esto es una ruta random");
+  res.send("Esto es una ruta random");
+});
+
+app.get("/get", (req, res) => {
+  console.log("Esto es un GET a '/get'");
+  res.send("Esto es un GET a '/get'");
+});
+
+app.get("/redirect", (req, res) => {
+  console.log("Esto es un GET a '/redirect'");
+    res.send("Esto es un GET a '/redirect'");
+    
 });
 
 app.listen(port, () => {
