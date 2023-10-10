e53244959cc2e8c795dcb0b1c06970e7446e4d1b
Leandro Loiacono <leandro@solidteam.es>
2023-10-10 10:29:01 +0200

    [FIX] '/redirect' route

--- src/index.js
+++ src/index.js
@@ -4,20 +4,19 @@ const app = express();
 
 const port = 3000;
 
-app.get("/*", (res, req) => {
-  console.log("Esto es una ruta random");
-  res.send("Esto es una ruta random");
-});
-
 app.get("/get", (req, res) => {
   console.log("Esto es un GET a '/get'");
   res.send("Esto es un GET a '/get'");
 });
 
 app.get("/redirect", (req, res) => {
-  console.log("Esto es un GET a '/redirect'");
-    res.send("Esto es un GET a '/redirect'");
-    
+  console.log("Esto es un GET a '/redirect', redirige a '/get");
+  res.redirect("/get");
+});
+
+app.get("/*", (req, res) => {
+  console.log("Esto es una ruta random");
+  res.send("Esto es una ruta random");
 });
 
 app.listen(port, () => {
